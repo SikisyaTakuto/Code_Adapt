9806888100ab78a8803b2ddff02589cd6f886c0c
SIKISYATAKUTO\tkond <tkondo591@gmail.com>
2026-02-03 09:04:10 +0900

    スライダーの修正

--- Assets/Script/AudioScript/VolumeController.cs
+++ Assets/Script/AudioScript/VolumeController.cs
@@ -4,30 +4,53 @@ using UnityEngine.UI;
 
 public class VolumeController : MonoBehaviour
 {
-    [SerializeField] private AudioMixer audioMixer; // 手順1で作ったMixerをアサイン
-    [SerializeField] private Slider bgmSlider;      // BGM用スライダーをアサイン
-    [SerializeField] private Slider seSlider;       // SE用スライダーをアサイン
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider bgmSlider;
+    [SerializeField] private Slider seSlider;
+
+    // 保存用のキー名
+    private const string BGM_KEY = "BGM_VOLUME";
+    private const string SE_KEY = "SE_VOLUME";
 
     private void Start()
     {
-        // スライダーの初期値を設定 (PlayerPrefsなどで保存している場合はそこから読み込む)
-        // AudioMixerのVolumeはデシベル(dB)なので、スライダーは0.0001?1の範囲にするのが一般的です
+        // 1. 保存されている値を取得（なければデフォルト値 1.0f）
+        float savedBGM = PlayerPrefs.GetFloat(BGM_KEY, 1.0f);
+        float savedSE = PlayerPrefs.GetFloat(SE_KEY, 1.0f);
+
+        // 2. スライダーの見た目を保存されていた値に合わせる
+        bgmSlider.value = savedBGM;
+        seSlider.value = savedSE;
+
+        // 3. AudioMixerにも値を適用する
+        SetBGMVolume(savedBGM);
+        SetSEVolume(savedSE);
+
+        // リスナー登録
         bgmSlider.onValueChanged.AddListener(SetBGMVolume);
         seSlider.onValueChanged.AddListener(SetSEVolume);
     }
 
     public void SetBGMVolume(float volume)
     {
-        // スライダーの値(0?1)をデシベル(-80?20)に変換
         audioMixer.SetFloat("BGMVolume", Mathf.Log10(Mathf.Max(0.0001f, volume)) * 20f);
+        // 値を保存
+        PlayerPrefs.SetFloat(BGM_KEY, volume);
     }
 
     public void SetSEVolume(float volume)
     {
         audioMixer.SetFloat("SEVolume", Mathf.Log10(Mathf.Max(0.0001f, volume)) * 20f);
+        // 値を保存
+        PlayerPrefs.SetFloat(SE_KEY, volume);
     }
 
-    // スライダーから手を離した時に呼び出す関数
+    private void OnDisable()
+    {
+        // シーン遷移時などに確実に保存を実行
+        PlayerPrefs.Save();
+    }
+
     public void OnSliderPointerUp()
     {
         if (SEManager.instance != null)
